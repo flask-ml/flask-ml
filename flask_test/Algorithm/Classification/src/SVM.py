1704662fa9cf03e1c35f5c55b83a1e4b9673463f
TwT520Ly <1131797361@qq.com>
2017-12-07 19:55:10 +0800

    1.2

--- flask_test/Algorithm/Classification/src/SVM.py
+++ flask_test/Algorithm/Classification/src/SVM.py
@@ -2,7 +2,7 @@ from sklearn import svm
 from sklearn.datasets import load_iris
 from flask_test.Algorithm.Classification.src.train_test import train_test_
 
-def main():
+def main(t):
     iris = load_iris()
     X = iris.data
     Y = iris.target
@@ -10,7 +10,7 @@ def main():
     kernel = ['linear', 'rbf', 'poly']
     C = 1.0
 
-    X_train, X_test, Y_train, Y_test = train_test_(X, Y, 0.2, 100)
+    X_train, X_test, Y_train, Y_test = train_test_(X, Y, t, 100)
 
     svc = svm.SVC(kernel='linear', C=C).fit(X_train,Y_train)
     print("SVC with linear kernel: ",svc.score(X_test,Y_test))
@@ -20,6 +20,7 @@ def main():
     print("SVC with Polynomial kernel: ",poly_svc.score(X_test,Y_test))
     lin_svc = svm.LinearSVC(C=C).fit(X_train,Y_train)
     print("Linear SVC: ",lin_svc.score(X_test,Y_test))
+    return svc.score(X_test, Y_test)
 
 if __name__ == '__main__':
     main()
